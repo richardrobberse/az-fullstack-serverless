64c516ce6615189a36b0f783a46cb645c79c3e15
Richard Robberse <richardrobberse@outlook.com>
2021-03-01 19:23:52 +0100

    Sort by completed and createdAt date.

--- api/graphql/resolvers/queries/todos.ts
+++ api/graphql/resolvers/queries/todos.ts
@@ -1,9 +1,17 @@
 import { InMemoryDatabase } from '../../lib/inMemoryDatabase'
 import { Todo } from '../../models/todo'
 
+const sortFn = (a: Todo, b: Todo) => {
+  if (b.completed && !a.completed) return -1
+
+  if ((b.completed && a.completed) || (!a.completed && !b.completed)) {
+    return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+  }
+
+  return 0
+}
+
 export default async (_: object, {}, context: any): Promise<Array<Todo>> => {
   const db = new InMemoryDatabase()
-  const ordered = db.getTodos().sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
-
-  return ordered
+  return db.getTodos().sort(sortFn)
 }
